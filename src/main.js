4bd28df03f84e6c3cff312883d02adab0fc10603
Ivan Cass <ivoasdf@gmail.com>
2023-03-17 19:15:40 -0300

    GUI ready. adding JS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-const sherlock = Sherlock;
 const sugar = Sugar.Date;
+const switchToggle = document.querySelector('.switch input');
+let delaysEvent = false;
 
-console.log('sherlock', sherlock);
-console.log('sugar', sugar);
+switchToggle.addEventListener('click', onSwitchToggleClick);
+
+if (chrome.storage) {
+  console.log('HAS');
+}
+
+if (chrome.storage)
+  chrome.storage.local
+    .get(['delaysEvent'])
+    .then((result) => (delaysEvent = result.key));
+
+function onSwitchToggleClick(e) {
+  delaysEvent = switchToggle.checked;
+
+  if (chrome.storage);
+  chrome.storage.local.set({ delaysEvent: switchToggle.checked });
+}
+
+function loadDelaySetting() {}
